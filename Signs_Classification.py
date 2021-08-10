3ef994204c010fcba03873cebd05f8688d2a7bd4
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-10 23:50:36 +0100

    Library Loading and DataViz

---
+++ Signs_Classification.py
@@ -0,0 +1,63 @@
+import math
+import numpy as np
+import h5py
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imread
+import scipy
+from PIL import Image
+import pandas as pd
+import tensorflow as tf
+import tensorflow.keras.layers as tfl
+from tensorflow.python.framework import ops
+from tensorflow.keras import activations
+
+# Loading the data (signs)
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_signs_dataset()
+
+# Example of an image from the dataset
+index = 9
+plt.imshow(X_train_orig[index])
+print ("y = " + str(np.squeeze(Y_train_orig[:, index])))
+
+# Functional API
+
+
+def convolutional_model(input_shape):
+    """
+    Implements the forward propagation for the model:
+    CONV2D -> RELU -> MAXPOOL -> CONV2D -> RELU -> MAXPOOL -> FLATTEN -> DENSE
+
+    Note that for simplicity and grading purposes, you'll hard-code some values
+    such as the stride and kernel (filter) sizes.
+    Normally, functions should take these values as function parameters.
+
+    Arguments:
+    input_img -- input dataset, of shape (input_shape)
+
+    Returns:
+    model -- TF Keras model (object containing the information for the entire training process)
+    """
+
+    input_img = tf.keras.Input(shape=input_shape)
+    ## CONV2D: 8 filters 4x4, stride of 1, padding 'SAME'
+    Z1 = tf.keras.layers.Conv2D(filters=8, kernel_size=(4, 4), strides=(1, 1), padding='same')(input_img)
+    ## RELU
+    A1 = tf.keras.layers.ReLU()(Z1)
+    ## MAXPOOL: window 8x8, stride 8, padding 'SAME'
+    P1 = tf.keras.layers.MaxPool2D(pool_size=(8, 8), strides=(8, 8), padding='same')(A1)
+    ## CONV2D: 16 filters 2x2, stride 1, padding 'SAME'
+    Z2 = tf.keras.layers.Conv2D(filters=16, kernel_size=(2, 2), padding='same')(P1)
+    ## RELU
+    A2 = tf.keras.layers.ReLU()(Z2)
+    ## MAXPOOL: window 4x4, stride 4, padding 'SAME'
+    P2 = tf.keras.layers.MaxPool2D(pool_size=(4, 4), strides=(4, 4), padding='same')(A2)
+    ## FLATTEN
+    F = tf.keras.layers.Flatten()(P2)
+    ## Dense layer
+    ## 6 neurons in output layer. Hint: one of the arguments should be "activation='softmax'"
+    outputs = tf.keras.layers.Dense(units=6, activation='softmax')(F)
+
+    model = tf.keras.Model(inputs=input_img, outputs=outputs)
+    return model
+
+
