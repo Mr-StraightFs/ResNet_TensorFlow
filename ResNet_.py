448d9ffbac12cc4ce542fcb6dff75b28e6485041
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-14 14:14:36 +0100

    add the identity block

--- ResNet_.py
+++ ResNet_.py
@@ -10,3 +10,47 @@ from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
 from tensorflow.python.framework.ops import EagerTensor
 from matplotlib.pyplot import imshow
+
+
+def identity_block(X, f, filters, training=True, initializer=random_uniform):
+    """
+    Implementation of the identity block as defined in Figure 4
+
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    training -- True: Behave in training mode
+                False: Behave in inference mode
+    initializer -- to set up the initial weights of a layer. Equals to random uniform initializer
+
+    Returns:
+    X -- output of the identity block, tensor of shape (m, n_H, n_W, n_C)
+    """
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value. You'll need this later to add back to the main path.
+    X_shortcut = X
+
+    # First component of main path
+    X = Conv2D(filters=F1, kernel_size=1, strides=(1, 1), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)  # Default axis
+    X = Activation('relu')(X)
+
+    ## Second component of main path
+    X = Conv2D(filters=F2, kernel_size=f, strides=(1, 1), padding='same', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)  # Default axis
+    X = Activation('relu')(X)
+
+    ## Third component of main path
+    X = Conv2D(filters=F3, kernel_size=1, strides=(1, 1), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)  # Default axis
+
+    ## Final step: Add shortcut value to main path, and pass it through a RELU activa
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return X
+
