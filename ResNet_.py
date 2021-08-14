78fb296270ee97aa39a24bb57c57098d20580fb8
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-14 14:03:58 +0100

    Load Libraries

---
+++ ResNet_.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+import numpy as np
+import scipy.misc
+from tensorflow.keras.applications.resnet_v2 import ResNet50V2
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_predictions
+from tensorflow.keras import layers
+from tensorflow.keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.initializers import random_uniform, glorot_uniform, constant, identity
+from tensorflow.python.framework.ops import EagerTensor
+from matplotlib.pyplot import imshow
