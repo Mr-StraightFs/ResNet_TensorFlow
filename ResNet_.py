fcdc932c6dd6477462cd23c4aa5d115af4a18b7f
Mr-StraightFs <amin.lh.mba@gmail.com>
2021-08-14 14:26:04 +0100

    Define the Convolution  Path

--- ResNet_.py
+++ ResNet_.py
@@ -54,3 +54,58 @@ def identity_block(X, f, filters, training=True, initializer=random_uniform):
 
     return X
 
+
+def convolutional_block(X, f, filters, s=2, training=True, initializer=glorot_uniform):
+    """
+    Implementation of the convolutional block as defined in Figure 4
+
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    s -- Integer, specifying the stride to be used
+    training -- True: Behave in training mode
+                False: Behave in inference mode
+    initializer -- to set up the initial weights of a layer. Equals to Glorot uniform initializer,
+                   also called Xavier uniform initializer.
+
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+
+    # Retrieve Filters
+    F1, F2, F3 = filters
+
+    # Save the input value
+    X_shortcut = X
+
+    ##### MAIN PATH #####
+
+    # First component of main path glorot_uniform(seed=0)
+    X = Conv2D(filters=F1, kernel_size=1, strides=(s, s), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)
+    X = Activation('relu')(X)
+
+    ### START CODE HERE
+
+    ## Second component of main path (≈3 lines)
+    X = Conv2D(filters=F2, kernel_size=f, strides=(1, 1), padding='same', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)
+    X = Activation('relu')(X)
+
+    ## Third component of main path (≈2 lines)
+    X = Conv2D(filters=F3, kernel_size=1, strides=(1, 1), padding='valid', kernel_initializer=initializer(seed=0))(X)
+    X = BatchNormalization(axis=3)(X, training=training)
+
+    ##### SHORTCUT PATH ##### (≈2 lines)
+    X_shortcut = Conv2D(filters=F3, kernel_size=1, strides=(s, s), padding='valid',
+                        kernel_initializer=initializer(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis=3)(X_shortcut, training=training)
+
+    ### END CODE HERE
+
+    # Final step: Add shortcut value to main path (Use this order [X, X_shortcut]), and pass it through a RELU activation
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+
+    return X
